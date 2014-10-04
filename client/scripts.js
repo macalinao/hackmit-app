a18695a1c60efeabb5c5f24ffc6927e72216975a
Ian Macalinao <ian@giza.us>
2014-10-04 14:02:03 -0500

    add rotating cube

--- client/scripts.js
+++ client/scripts.js
@@ -6,14 +6,36 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({
+var material = new THREE.MeshLambertMaterial({
   color: 0x00ff00
 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+// create a point light
+var pointLight =
+  new THREE.PointLight(0xFFFFFF);
+
+// set its position
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+
+// add to the scene
+scene.add(pointLight);
+
 camera.position.z = 5;
+
+var angularSpeed = 0.2;
+var lastTime = 0;
 function render() {
+  // update
+  var time = (new Date()).getTime();
+  var timeDiff = time - lastTime;
+  var angleChange = angularSpeed * timeDiff * 2 * Math.PI / 1000;
+  cube.rotation.y += angleChange;
+  lastTime = time;
+
   requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
