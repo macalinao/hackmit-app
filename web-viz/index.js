2f688de12bb16a14876a8a513a84ef9e0095fa6c
Ian Macalinao <ian@giza.us>
2014-10-04 23:59:21 -0500

    blackity black

--- web-viz/index.js
+++ web-viz/index.js
@@ -456,8 +456,8 @@ function createApp() {
       centerAirline.app = app;
       airlineMgr.app = app;
 
-      gl.clearColor(0.1, 0.1, 0.1, 1);
-      gl.clearDepth(1);
+      gl.clearColor(0, 0, 0, 0);
+      gl.clear(clearOpt);
       gl.enable(gl.DEPTH_TEST);
       gl.depthFunc(gl.LEQUAL);
 
